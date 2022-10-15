f8d37c985e5816d30d2fa860c0e2a39ec224db8d
OlegKhudoerko <292oleg292@mail.ru>
2022-10-15 18:22:35 +0300

    task_6_1.py finish

--- task_6_1.py
+++ task_6_1.py
@@ -1,4 +1,4 @@
-# 1. Представлен список чисел. Необходимо вывести элементы исходного списка, 
+# 1. Представлен список чисел. Необходимо вывести элементы исходного списка,
 # значения которых больше предыдущего элемента. Use comprehension.
 # in
 # 9
@@ -14,3 +14,19 @@
 # [28, 20, 10, 5, 1, 24, 7, 15, 23, 25]
 # [24, 15, 23, 25]
 
+from random import sample
+
+
+def f_rnd(num):
+    if not num.isnumeric():
+        print("The data is incorrect")
+        return []
+    return sample(range(int(num)*3), int(num))
+
+
+def ls_more(num):
+    return [num[i] for i in range(1, len(num)) if num[i] > num[i - 1]]
+
+
+ls = f_rnd(input("Введите количество чисел: "))
+print(f"{ls} \n \r{ls_more(ls)}")
